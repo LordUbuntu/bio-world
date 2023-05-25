87eef935e3c9883d67355e9b2f332c626f95c315
Jacobus Burger <therealjacoburger@gmail.com>
2023-05-24 23:00:55 -0700

    init commit

---
+++ bioworld.py
@@ -0,0 +1,32 @@
+# TODO:
+# - store enemy and ally art somewhere (probably some arrays)
+# - find a way to store game data
+# - implement gameplay
+# - implement ascii art
+
+
+
+
+def bioworld():
+    # render menu
+    # get player choice for menu
+        # if quit, clear screen and terminate program
+        # if load game
+            # load game data from file
+            # render game
+        # if new game, start a new game
+            # render game
+                # while in game
+                    # render stats
+                    # render art (enemy, room, etc)
+                    # render description
+                    # show choices
+                    # get user input
+                        # perform action based on user input
+    # win condition: turn the 7 machine lords into bio-machine hybrids
+    # lose condition: lose the bio-virus/entity within you (health <= 0)
+
+
+
+if __name__ == '__main__':
+    bioworld()
