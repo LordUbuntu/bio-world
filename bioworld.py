e99f936e76dd160ffc6f27e05705731fd217db76
Jacobus Burger <therealjacoburger@gmail.com>
2023-05-25 22:55:36 -0700

    added screen clearing, menu render and art, and input placeholders

--- bioworld.py
+++ bioworld.py
@@ -5,7 +5,21 @@
 # - add a dict to store and access art
 # - add functions representing abilities, maybe store in dict too
 # - make enemy class
+import os
+
+
 ART = {
+    "menu": r"""
+     ____ ___ _____        _____  ____  _     ____
+    | __ )_ _/ _ \ \      / / _ \|  _ \| |   |  _ \
+    |  _ \| | | | \ \ /\ / / | | | |_) | |   | | | |
+    | |_) | | |_| |\ V  V /| |_| |  _ <| |___| |_| |
+    |____/___\___/  \_/\_/  \___/|_| \_\_____|____/
+
+               ==========================
+                            .
+               ==========================
+    """,
     "enemy": """
     \\\\\\       ///
 
@@ -16,6 +30,15 @@ ART = {
       >=======<
     """
 }
+
+
+# NOTE: screen must be cleared after each input
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def render_menu():
+    print(ART["menu"])
 
 
 def render_game(state):
@@ -32,6 +55,9 @@ def render_game(state):
 
 
 def bioworld():
+    render_menu()
+    input()
+    clear()
     # render menu
     # get player choice for menu
         # if quit, clear screen and terminate program
@@ -41,9 +67,11 @@ def bioworld():
         # if new game, start a new game
             # while in game
                 # render game
-                render_game({"health": 10, "power": 5})
+    render_game({"health": 10, "power": 5})
                 # get user input
+    input()  # placeholder for now
                     # perform action based on user input
+    clear()
     # win condition: turn the 7 machine lords into bio-machine hybrids
     # lose condition: lose the bio-virus/entity within you (health <= 0)
 
