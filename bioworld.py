0a655f62048809d599c1826102c6f80898ec1bc1
Jacobus Burger <therealjacoburger@gmail.com>
2023-05-25 17:13:04 -0700

    added game renderer

--- bioworld.py
+++ bioworld.py
@@ -1,3 +1,9 @@
+# try to load rich module for colours if available
+try:
+    import rich
+    print = rich.print
+except ModuleNotFoundError:
+    pass
 # TODO:
 # - store enemy and ally art somewhere (probably some arrays)
 # - find a way to store game data
@@ -5,6 +11,18 @@
 # - implement ascii art
 
 
+
+
+def render_game(state):
+    # render stats
+    print('=' * 20)
+    print("health: [red]{}[/red]   power: [cyan]{}[/cyan]"\
+            .format(state["health"], state["power"]))
+    # render art
+    print('=' * 20)
+    # show choices
+    print('=' * 20)
+    print('=' * 20)
 
 
 def bioworld():
@@ -15,14 +33,11 @@ def bioworld():
             # load game data from file
             # render game
         # if new game, start a new game
-            # render game
-                # while in game
-                    # render stats
-                    # render art (enemy, room, etc)
-                    # render description
-                    # show choices
-                    # get user input
-                        # perform action based on user input
+            # while in game
+                # render game
+                render_game({"health": 10, "power": 5})
+                # get user input
+                    # perform action based on user input
     # win condition: turn the 7 machine lords into bio-machine hybrids
     # lose condition: lose the bio-virus/entity within you (health <= 0)
 
